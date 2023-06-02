f1a9e0dfe24f5ba58ac9b8f474630a8aec34daa3
Chertanov <yaroslav.chertanov@nure.ua>
2023-06-02 21:23:06 +0300

    Added some templates for user pages

---
+++ client/pages/profile.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <title>Sign Up</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="http://quiz-game/client/src/css/main_pages.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+    </style>        
+</head>
+
+<body>
+
+
+<header>
+    <div id="header"></div>
+</header>
+
+<div style="display: flex;">
+  <div style="flex: 1;" id="first-block">
+  <button id="home-button">Home</button>
+    </div>
+  <div style="flex: 5;" id="second-block">
+    </div>
+</div>
+
+<script>
+    $("#header").load("widgets/header.html");
+</script>
+    
+
+</html>
