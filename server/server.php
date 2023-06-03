afacbfe6df7f7f09f13d56b0f45731621ae1570d
Yevhen Tukubaiev <yevhen.tukubaiev@nure.ua>
2023-06-03 14:58:16 +0300

    Add basic db connection logic
    Signed-off-by: Yevhen Tukubaiev <yevhen.tukubaiev@nure.ua>

--- server/server.php
+++ server/server.php
@@ -1,1 +1,16 @@
-//Server Start and HTTP routing here
+<?php
+
+require_once '../vendor/autoload.php';
+require_once 'services/PDOConnection.php';
+
+use Server\Services\PDOConnection;
+
+header('Access-Control-Allow-Origin: http://brigade-one-quiz-game');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+$connection = new PDOConnection('localhost', 'quiz_db', 'root', null);
+
+// Listen for incoming client requests
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'];
