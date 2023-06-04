a895c33d327906391dbccb884b9761e847a88c6e
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-06-04 13:16:35 +0300

    some AuthPages UI enhances

--- client/pages/sign_in.php
+++ client/pages/sign_in.php
@@ -1,12 +1,12 @@
 <html>
 
 <head>
-    <title>Sign In</title>
+    <title>Log In | Quiz</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="http://quiz-game/client/src/css/sign_pages.css">
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
-    </style>        
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+    </style>
 </head>
 
 <body>
@@ -14,29 +14,29 @@
         <div id="header"></div>
     </header>
     <div id="container-block">
-    <p id='main-font'>Log In to your Account</p>
-    <p class='faded-font'>with your registered Email Address</p>
-    <br>
-    
-
-    <form id="sign-form" method="post">
-
-        <label for="email">Email Address:</label>
-        <input type="email" name="email" name="email"  required>
+        <p id='main-font'>Log In to your Account</p>
+        <p class='faded-font'>with your registered Email Address</p>
         <br>
 
-        <label for="password">Enter password:</label>
-        <input type="password" name="password" id="password"
-            required pattern=".{6,}">
-        <br>
 
-        <a class="faded-font" href='http://quiz-game/client/pages/sign_up.php'>Don't have an account yet?</a>
-        <br>
-        <input type="submit" value="Login">
-        <div id="status">
+        <form id="sign-form" method="post">
 
-        </div>
-    </form>
+            <label for="email">Email Address:</label>
+            <input type="email" name="email" name="email" placeholder="Enter your email address" required>
+            <br>
+
+            <label for="password">Enter password:</label>
+            <input type="password" name="password" id="password" placeholder="Enter your password" required
+                pattern=".{6,}">
+            <br>
+
+            <a class="faded-font" href='http://quiz-game/client/pages/sign_up.php'>Don't have an account yet? Sign Up</a>
+            <br>
+            <input type="submit" value="Login">
+            <div id="status">
+
+            </div>
+        </form>
     </div>
     <script type="module" src="../src/js/sign_in.js"></script>
 
@@ -46,6 +46,6 @@
         </div>
     </footer>
 
-    
+
 
 </html>
--- client/pages/sign_up.php
+++ client/pages/sign_up.php
@@ -1,12 +1,12 @@
 <html>
 
 <head>
-    <title>Sign Up</title>
+    <title>Sign Up | Quiz</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="http://quiz-game/client/src/css/sign_pages.css">
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
-    </style>        
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+    </style>
 </head>
 
 <body>
@@ -16,34 +16,36 @@
         <div id="header"></div>
     </header>
     <div id="container-block">
-    <p id='main-font'>Register your Account</p>
-    <br>
-
-    <form id="sign-form" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username"  required>
+        <p id='main-font'>Create an account</p>
+        <p class='faded-font'>Sign Up by entering fields below</p>
         <br>
 
-        <label for="email">Email address:</label>
-        <input type="email" name="email" name="email"  required>
-        <br>
+        <form id="sign-form" method="post">
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username" placeholder="Enter desired username" required>
+            <br>
 
-        <label for="password">Enter password:</label>
-        <input type="password" name="password" id="password"
-            required pattern=".{6,}">
-        <br>
+            <label for="email">Email address:</label>
+            <input type="email" name="email" name="email" placeholder="Enter an email address" required>
+            <br>
 
-        <label for="confirmPassword">Confirm Password:</label>
-        <input type="password" name="confirmPassword" id="confirmPassword"  required>
-        <br>
+            <label for="password">Enter password:</label>
+            <input type="password" name="password" id="password" placeholder="Enter a password" required
+                pattern=".{6,}">
+            <br>
 
-        <a class="faded-font" href='http://quiz-game/client/pages/sign_in.php'>Already have one?</a>
-        <br>
-        <input type="submit" value="Register">
-        <div id="status">
+            <label for="confirmPassword">Confirm Password:</label>
+            <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Repeat the password" required
+                pattern=".{6,}">
+            <br>
 
-        </div>
-    </form>
+            <a class="faded-font" href='http://quiz-game/client/pages/sign_in.php'>Already have one?Log In</a>
+            <br>
+            <input type="submit" value="Sign Up">
+            <div id="status">
+
+            </div>
+        </form>
     </div>
     <script type="module" src="../src/js/sign_up.js"></script>
 
@@ -53,6 +55,6 @@
         </div>
     </footer>
 
-    
+
 
 </html>
