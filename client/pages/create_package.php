8f2eeff4d14b3a73f368091e65bc49e7ab6ea239
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-06-05 12:32:32 +0300

    create package page enhances

--- client/pages/create_package.php
+++ client/pages/create_package.php
@@ -1,80 +1,91 @@
 <html>
 
 <head>
-    <title>Sign Up</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="http://quiz-game/client/src/css/main_pages.css">
-    <link rel="stylesheet" href="http://quiz-game/client/src/css/create.css">
-    <style>
+  <title>Create a package | Quiz</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <link rel="stylesheet" href="http://quiz-game/client/src/css/main_pages.css">
+  <link rel="stylesheet" href="http://quiz-game/client/src/css/create.css">
+  <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
-    </style>        
+  </style>
 </head>
 
 <body>
 
 
-<header>
+  <header>
     <div id="header"></div>
-</header>
+  </header>
 
-<div style="display: flex;">
-  <div style="flex: 1;" id="first-block">
-  <button onclick="window.location.href='http://quiz-game/client/pages/index.php'" id="home-button">
-    <img src="http://quiz-game/client/assets/images/home_button_image.jpg" alt="Image">
-    <span class="button-text">Home</span>
-  </button>
-  <button id="profile-button">
-    <img src="http://quiz-game/client/assets/images/profile_2.jpg" alt="Image">
-    <span class="button-text">Profile</span>
-  </button>
+  <div style="display: flex;">
+    <div style="flex: 1;" id="first-block">
+      <button onclick="window.location.href='http://quiz-game/client/pages/index.php'" id="home-button">
+        <img src="http://quiz-game/client/assets/images/home_button_image.jpg" alt="Image">
+        <span class="button-text">Home</span>
+      </button>
+      <button id="profile-button">
+        <img src="http://quiz-game/client/assets/images/profile.png" alt="Image">
+        <span class="button-text">Profile</span>
+      </button>
     </div>
 
-  <div style="flex: 5;" id="second-block">
-  <div  id="home">
-    <p><b>Create a new package</b>
-    </div>
-    <div id="package_name">
-    <hr>
-        <label for="package_name_value">Package name</label>
-        <br>
-        <input type="text" id="package_name_value" placeholder="Enter package name"></input>
-    </div>
-    <div id="question">
-      <div class="question_info">
-        <label for="question_text">Add questions</label>
-        <br>
-        <input type="text" id="question_text_value" placeholder="Enter question text"></input> 
-        <div style="display:flex">
-        <div style="flex:49.5;width:10%;">
-        <input type="text"id="answer_value" placeholder="Enter answer"></input><span>          </span><input type="text" placeholder="Enter hint" id="hint_value"></input>
-        
-        </div>
-        <div style="flex:50.5;">
-        </div>
-        </div>
+    <div style="flex: 5;" id="second-block">
+      <div id="home">
+        <p><b>Create a new package</b>
+      </div>
+      <div id="package_name">
         <hr>
-        </div>
-  </div>
-  <button id="new-button">
-    
-    <span class="button-text">+ Add a question</span>
-  </button>
-    <div id="creation">
-    <button id="create-button">
-    
-    <span class="button-text">Create</span>
-  </button>
-    </div>
-</div>
-</div>
+        <label for="package_name_value">Package name</label>
+        <br> 
+        <input type="text" id="package_name_value" placeholder="Enter package name"></input>
+      </div>
+      <div id="question"><label for="question_text">Add questions</label>
+        <div class="question_info">
+          <h6 class="question_number">#1</h6>
+          <input type="text" id="question_text_value" placeholder="Enter question text"></input>
+          <div style="display:flex">
+            <div style="flex:49.5;width:10%;">
+              <input type="text" id="answer_value" placeholder="Enter answer"></input><span> </span><input type="text"
+                placeholder="Enter hint" id="hint_value"></input>
 
-<script>
+            </div>
+            <div style="flex:50.5;">
+            </div>
+          </div>
+          <hr>
+        </div>
+      </div>
+      <button id="new-button">
+        <span class="button-text">+ Add a question</span>
+      </button>
+      <div id="creation">
+        <button id="create-button">
+          <span class="button-text">Create</span>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <script>
     $("#header").load("widgets/header.html");
 
-    $("#new-button").on("click", function () {
-            $("#question").html($("#question").html() + $(".question_info").html());
-        });
-</script>
-    
+    // Add a click event listener to the add question button
+    $("#new-button").click(function () {
+  var clonedQuestionInfo = $(".question_info:first").clone();
+  var existingQuestionInfo = $(".question_info");
+  var totalQuestions = existingQuestionInfo.length;
+  var questionNumber = clonedQuestionInfo.find(".question_number");
+
+  // Clear input values
+  clonedQuestionInfo.find("input[type='text']").val("");
+
+  questionNumber.text("#" + (totalQuestions + 1));
+  $("#question").append(clonedQuestionInfo);
+});
+
+
+
+  </script>
+
 
 </html>
