125d95a4217ad25933c4dcaf57e9ad674d3fcf18
Yevhen Tukubaiev <yevhen.tukubaiev@nure.ua>
2023-06-07 12:29:12 +0300

    Use json & fix routing callback issues & little refactor
    Signed-off-by: Yevhen Tukubaiev <yevhen.tukubaiev@nure.ua>

--- server/server.php
+++ server/server.php
@@ -17,115 +17,138 @@ header('Access-Control-Allow-Origin: http://brigade-one-quiz-game');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
+// Listen for incoming client requests
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'];
 
 $router = new HttpRouter();
-$db = new Database(
+$conn = (new Database(
     new PDO('mysql:host=localhost;dbname=quiz_db', 'root', null)
-);
+))->getConnection();
 
-$router->addRoute('POST', '/sign_up', function () use ($db) {
+if ($method != 'GET') {
+    $post_data = json_decode(file_get_contents('php://input'));
+}
+
+$router->addRoute('POST', '/sign_up', function () use ($conn, $post_data) {
     $ur = new UserRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
+
     $user = new User(
         null,
-        $_POST['name'],
-        $_POST['email'],
-        $_POST['password'],
+        $post_data->user,
+        $post_data->email,
+        $post_data->password,
         UserRole::RegularUser
     );
+
     if ($ur->create($user)) {
-        echo json_encode($user);
+        echo $user->toJSON();
     }
-    return false;
 });
 
-$router->addRoute('POST', '/sign_in', function () use ($db) {
+$router->addRoute('POST', '/sign_in', function () use ($conn, $post_data) {
     $ur = new UserRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
-    $user = $ur->fetchByEmail($_POST['email']);
-    echo ($user->getPassword() === $_POST['password'])
-        ? json_encode($user->toJSON())
+
+    $user = $ur->fetchByEmail($post_data->email);
+
+    echo ($user->getPassword() === $post_data->password)
+        ? $user->toJSON()
         : null;
 });
 
-$router->addRoute('PUT', '/user', function () use ($db) {
+$router->addRoute('PUT', '/user', function () use ($conn, $post_data) {
     $ur = new UserRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
-    $user = new User(
-        $_POST['id'],
-        $_POST['name'],
-        $_POST['email'],
-        $_POST['password'],
-        UserRole::from($_POST['roleId'])
-    );
+
+    $user = $ur->fetchById($post_data->userID);
+
+    $user->setEmail($post_data->email);
+    $user->setPassword($post_data->password);
+    $user->setName($post_data->Name);
+
     $ur->update($user);
 });
 
-$router->addRoute('DELETE', '/user', function () use ($db) {
+$router->addRoute('DELETE', '/user', function () use ($conn, $post_data) {
     $ur = new UserRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
-    $user = $ur->fetchById($_POST['id']);
-    return $ur->delete($user);
+
+    $user = $ur->fetchById($post_data->userID);
+
+    $ur->delete($user);
 });
 
-$router->addRoute('POST', '/package', function () use ($db) {
+$router->addRoute('POST', '/package', function () use ($conn, $post_data) {
+
     $pr = new PackageRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
+        new IDGenerator()
+    );
+    $pqlr = new PackageQuestionLinkRepository(
+        new QueryExecutor($conn),
         new IDGenerator()
     );
 
-    $package = new Package(null, $_POST['name'], false);
+    $package = new Package(null, $post_data->name, false);
     $pr->create($package);
+
+    $pql = new PackageQuestionLink(null, $package->getPackageID(), '');
+    foreach ($post_data->questionsIDs as $questionID) {
+        $pql->setQuestionID($questionID);
+        $pqlr->create($pql);
+    }
 });
 
-$router->addRoute('PUT', '/package', function () use ($db) {
+$router->addRoute('PUT', '/package', function () use ($conn, $post_data) {
     $pr = new PackageRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
-    $package = $pr->fetchByID($_POST['packageID']);
-    $package->setIsApproved($_POST['isApproved']);
-    $package->setName($_POST['name']);
+
+    $package = $pr->fetchByID($post_data->packageID);
+    $package->setIsApproved($post_data->isApproved);
+    $package->setName($post_data->name);
 
     $pr->update($package);
 });
 
-$router->addRoute('DELETE', '/package', function () use ($db) {
+$router->addRoute('DELETE', '/package', function () use ($conn, $post_data) {
     $pr = new PackageRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
 
-    $package = $pr->fetchByID($_POST['packageID']);
+    $package = $pr->fetchByID($post_data->packageID);
+
     $pr->delete($package);
 });
 
-$router->addRoute('GET', '/package', function () use ($db) {
+$router->addRoute('GET', '/package', function () use ($conn, $post_data) {
     $pr = new PackageRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
-    echo $pr->fetchByID($_GET['packageID'])->toJSON();
+
+    echo $pr->fetchByID($post_data->packageID)->toJSON();
 });
 
-$router->addRoute('GET', '/public_packages', function () use ($db) {
+$router->addRoute('GET', '/public_packages', function () use ($conn, $post_data) {
     $pr = new PackageRepository(
-        new QueryExecutor($db->getConnection()),
+        new QueryExecutor($conn),
         new IDGenerator()
     );
+
     echo json_encode($pr->fetchPublicPackages());
 });
-
-// Listen for incoming client requests
-$method = $_SERVER['REQUEST_METHOD'];
-$path = $_SERVER['PATH_INFO'];
 
 $router->route($method, $path);
