aa648675d4297f3a8e688b38f91635e9ead2f052
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-06-08 08:15:09 +0300

    changed page UI

--- client/pages/manage.php
+++ client/pages/manage.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-  <title>Create a package | Quiz</title>
+  <title>Manage a package | Quiz</title>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <link rel="stylesheet" href="http://quiz-game/client/src/css/main_pages.css">
   <link rel="stylesheet" href="http://quiz-game/client/src/css/create.css">
@@ -9,8 +9,8 @@
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
   </style>
   <script>
-  const PackageID = <?php echo json_encode($_GET["packageID"]); ?>;
-  const Pack_name = <?php echo json_encode($_GET["pack_name"]); ?>;
+    const PackageID = <?php echo json_encode($_GET["packageID"]); ?>;
+    const Pack_name = <?php echo json_encode($_GET["pack_name"]); ?>;
   </script>
 </head>
 
@@ -23,17 +23,17 @@
 
   <div style="display: flex;">
     <div style="flex: 1;" id="first-block">
-      
+
     </div>
 
     <div style="flex: 5;" id="second-block">
       <div id="home">
-        <p><b>Create a new package</b>
+        <p><b>Manage package</b>
       </div>
       <div id="package_name">
         <hr>
-        <label for="package_name_value"  id="package_name_label">Package name</label>
-        <br> 
+        <label for="package_name_value" id="package_name_label">Package name</label>
+        <br>
         <input type="text" id="package_name_value" placeholder="Enter package name"></input>
       </div>
       <div id="question"><label for="question_text">Change questions</label>
@@ -42,8 +42,8 @@
           <input type="text" class="question_text_value" id="text1" placeholder="Enter question text" required></input>
           <div style="display:flex">
             <div style="flex:49.5;width:10%;">
-              <input type="text" class="answer_value" id="ans1" placeholder="Enter answer" required></input><span> </span><input type="text"
-                placeholder="Enter hint" id="hint1" class="hint_value"></input>
+              <input type="text" class="answer_value" id="ans1" placeholder="Enter answer" required></input><span>
+              </span><input type="text" placeholder="Enter hint" id="hint1" class="hint_value"></input>
             </div>
             <div style="flex:50.5;">
             </div>
@@ -51,10 +51,10 @@
           <hr>
         </div>
       </div>
-      
+
       <div id="creation">
-        <input type=submit id="create-button" value="Create">
-         
+        <input type=submit id="create-button" value="Update">
+
         </input>
       </div>
     </div>
@@ -62,23 +62,23 @@
 
   <script>
     $("#header").load("widgets/header.html");
-      $(document).ready(function () {
-        $("#first-block").load("widgets/nav_buttons.html");
+    $(document).ready(function () {
+      $("#first-block").load("widgets/nav_buttons.html");
     });
 
     // Add a click event listener to the add question button
     $("#new-button").click(function () {
-  var clonedQuestionInfo = $(".question_info:first").clone();
-  var existingQuestionInfo = $(".question_info");
-  var totalQuestions = existingQuestionInfo.length;
-  var questionNumber = clonedQuestionInfo.find(".question_number");
+      var clonedQuestionInfo = $(".question_info:first").clone();
+      var existingQuestionInfo = $(".question_info");
+      var totalQuestions = existingQuestionInfo.length;
+      var questionNumber = clonedQuestionInfo.find(".question_number");
 
-  // Clear input values
-  clonedQuestionInfo.find("input[type='text']").val("");
+      // Clear input values
+      clonedQuestionInfo.find("input[type='text']").val("");
 
-  questionNumber.text("#" + (totalQuestions + 1));
-  $("#question").append(clonedQuestionInfo);
-  });
+      questionNumber.text("#" + (totalQuestions + 1));
+      $("#question").append(clonedQuestionInfo);
+    });
 
 
   </script>
