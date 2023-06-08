167f2609a552ce6dea8866696610338bfb6df1b7
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-06-08 08:30:12 +0300

    user_competition_accuracy added
    user_competition_accuracy

--- server/server.php
+++ server/server.php
@@ -14,6 +14,7 @@ use Server\Repository\IDGenerator;
 use Server\Repository\PackageQuestionLinkRepository;
 use Server\Repository\UserPackageLinkRepository;
 use Server\Repository\TrainingHistoryRepository;
+use Server\Repository\CompetitionHistoryRepository;
 use Server\Repository\UserRepository;
 use Server\Repository\PackageRepository;
 use Server\Services\HttpRouter;
@@ -92,16 +93,28 @@ $router->addRoute('GET', '/user_packages', function () use ($conn, $json) {
     }
     echo json_encode($packagesJSON);
 });
+
 $router->addRoute('GET', '/user_training_accuracy', function () use ($conn, $json) {
     $thr = new TrainingHistoryRepository(
         new QueryExecutor($conn),
         new IDGenerator()
     );
     $userID = $_GET['userID'];
-    $trainingAccuracy = $thr->fetchUserTrainingAccuracyByUserID($userID);
+    $accuracy = $thr->fetchUserTrainingAccuracyByUserID($userID);
 
     echo json_encode([
-        'trainingAccuracy' => $trainingAccuracy
+        'accuracy' => $accuracy
+    ]);
+});
+$router->addRoute('GET', '/user_competition_accuracy', function () use ($conn, $json) {
+    $thr = new CompetitionHistoryRepository(
+        new QueryExecutor($conn),
+        new IDGenerator()
+    );
+    $userID = $_GET['userID'];
+    $accuracy = $thr->fetchUserCompetitionAccuracyByUserID($userID);
+    echo json_encode([
+        'accuracy' => $accuracy
     ]);
 });
 
