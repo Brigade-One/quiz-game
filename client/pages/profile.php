3df749ff74de748381ca60fc582b02ffe69fdf27
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-06-08 07:49:40 +0300

    enhanced profile page

--- client/pages/profile.php
+++ client/pages/profile.php
@@ -8,7 +8,7 @@
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
   </style>
-  
+
 </head>
 
 <body>
@@ -23,85 +23,104 @@
 
     <div style="flex: 5" id="second-block">
 
-      <div style="display: flex;height=200px;">
-        <div style="flex: 1;">
-          <img src="http://quiz-game/client/assets/images/profile_image.jpg" alt="Image" id="profile-picture">
-        </div>
 
-        <div style="flex: 4;height=200px;">
-          <div class="top">
-            <b><p id="profile_name">Anonymous</b>
-              <br><span style="font-size:15px;">Guest</span>
-            </p>
-          </div>
+      <div style="display: flex;">
+        <div style="flex: 1;" id="first-block"> </div>
 
-          <div class="bottom" style="display: flex;">
+        <div style="flex: 5" id="second-block">
+
+          <div style="display: flex;height=200px;">
             <div style="flex: 1;">
+              <img src="http://quiz-game/client/assets/images/profile_image.jpg" alt="Image" id="profile-picture">
             </div>
-            <div style="flex: 1;" id="fastest-time">
-              <img src="http://quiz-game/client/assets/images/vector.jpg" alt="Image" class="flag">
-              <span id="profile_name"><b>27min</b>
-                <br>
-                <p style="font-size:15px;">Fastest time</p>
+
+            <div style="flex: 4;height=200px;">
+              <div class="top">
+                <p id="profile_name"><b>Oluwatobi Olowu</b>
+                  <br><span style="font-size:15px;">Examiner</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div style="display: flex;position:relative;margin-top:30px;">
+            <div style="flex: 1;" class="info-block">
+              <div class="square">
+                <img src="http://quiz-game/client/assets/images/check.png" alt="Image" class="icon">
+              </div>
+              <span id="profile_name"><b>89.28%</b> <br>
+                <p style="font-size:15px;">Training accuracy</p>
               </span>
             </div>
-            <div style="flex: 1;" id="correct-answers">
-              <img src="http://quiz-game/client/assets/images/vector.jpg" alt="Image" class="flag">
-              <span id="profile_name"><b>200</b>
-                <br>
-                <p style="font-size:15px;">Correct Answers</p>
+            <div style="flex: 1;" class="info-block">
+              <div class="square">
+                <img src="http://quiz-game/client/assets/images/check.png" alt="Image" class="icon">
+              </div>
+              <span id="profile_name"><b>76.28%</b> <br>
+                <p style="font-size:15px;">Competition accuracy</p>
               </span>
             </div>
           </div>
+          <div style="display: flex;position:relative;margin-top:30px;">
+            <div style="flex: 1;" class="info-block">
+              <div class="square">
+                <img src="http://quiz-game/client/assets/images/package.png" alt="Image" class="icon">
+              </div>
+              <span id="profile_name"><b>3</b> <br>
+                <p style="font-size:15px;">Packages created</p>
+              </span>
+            </div>
+            <div style="flex: 1;" class="info-block">
+              <div class="square">
+                <img src="http://quiz-game/client/assets/images/brown_flag.png" alt="Image" class="icon">
+              </div>
+              <span id="profile_name"><b>3</b> <br>
+                <p style="font-size:15px;">1x1 Competitions Wins</p>
+              </span>
+            </div>
+          </div>
+          <div style="display: flex;position:relative;margin-top:30px;">
+            <div style="flex: 1;" class="info-block">
+              <div class="square">
+                <img src="http://quiz-game/client/assets/images/timer.png" alt="Image" class="icon">
+              </div>
+              <span id="profile_name"><b>27 May 2023</b> <br>
+                <p style="font-size:15px;">Last Training date</p>
+              </span>
+            </div>
+          </div>
+          <button id="logout">
+            <img src="http://quiz-game/client/assets/images/logout_white.png" alt="Image">
+            <span class="button-text">Log Out</span>
+          </button>
         </div>
-      </div>
 
-      <div style="display: flex;position:relative;margin-top:30px;">
-        <div style="flex: 1;" class="flag-result">
-          <img src="http://quiz-game/client/assets/images/gray_flag.png" alt="Image" class="flag">
-          <span id="profile_name"><b>200</b> <br>
-            <p style="font-size:15px;">Correct Answers</p>
-          </span>
-        </div>
-        <div style="flex: 1;" class="flag-result">
-          <img src="http://quiz-game/client/assets/images/brown_flag.png" alt="Image" class="flag">
-          <span id="profile_name"><b>200</b> <br>
-            <p style="font-size:15px;">Correct Answers</p>
-          </span>
-        </div>
-      </div>
-      <button id="logout">
-        <img src="http://quiz-game/client/assets/images/logout.png" alt="Image">
-        <span class="button-text">Log Out</span>
-      </button>
-    </div>
+        <script>
+          $("#header").load("widgets/header.html");
+          $(document).ready(function () {
+            $("#first-block").load("widgets/nav_buttons.html");
+          });
 
-    <script>
-      $("#header").load("widgets/header.html");
-      $(document).ready(function () {
-        $("#first-block").load("widgets/nav_buttons.html");
-      });
+          let user_name_storage_c = localStorage.getItem("username");
+          let status_numeral = localStorage.getItem("role");
+          let user_status = "Guest";
+          if (status_numeral == 1) {
+            user_status = "Examiner";
+          }
+          if (status_numeral == 0) {
+            user_status = "Regular User";
+          }
+          if (user_name_storage_c != "Guest") {
+            if (user_name_storage_c != null) {
+              document.getElementById("profile_name").innerHTML = user_name_storage_c + '</b><br><span style="font-size:15px;">' + user_status + '</span>';
+            }
+          }
 
-        let user_name_storage_c = localStorage.getItem("username");
-        let status_numeral = localStorage.getItem("role");
-        let user_status = "Guest";
-        if(status_numeral == 1){
-          user_status = "Examiner";
-        }
-        if(status_numeral == 0){
-          user_status = "Regular User";
-        }
-        if(user_name_storage_c != "Guest"){
-            if(user_name_storage_c != null){
-                document.getElementById("profile_name").innerHTML = user_name_storage_c+'</b><br><span style="font-size:15px;">'+user_status+'</span>';
-            }             
-        }
-
-      $("#logout").on("click", function () {
+          $("#logout").on("click", function () {
             localStorage.clear();
             window.location.href = "http://quiz-game/client/pages/sign_in.php";
-        });
-    </script>
+          });
+        </script>
 
 
 </html>
